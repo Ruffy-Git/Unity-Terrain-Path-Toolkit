bf54db1ec2a341390b7fd081ca870e84ee241ed2
Ruffy-Git <sabastianlower33@gmail.com>
2026-09-22 11:10:43 -0500

    Add mesh fingerprint test

--- Tests/Editor/PathMeshBuilderTests.cs
+++ Tests/Editor/PathMeshBuilderTests.cs
@@ -12,16 +12,23 @@ namespace TerrainPathToolkit.Tests
             var go = new GameObject("Path Test");
             try
             {
-                var points = new List<Vector3> { Vector3.zero, Vector3.forward * 5f };
-                var mesh = PathMeshBuilder.BuildStrip(points, 2f, go.transform);
-
+                var mesh = PathMeshBuilder.BuildStrip(new List<Vector3> { Vector3.zero, Vector3.forward * 5f }, 2f, go.transform);
                 Assert.AreEqual(4, mesh.vertexCount);
                 Assert.AreEqual(6, mesh.triangles.Length);
             }
-            finally
-            {
-                Object.DestroyImmediate(go);
-            }
+            finally { Object.DestroyImmediate(go); }
+        }
+
+        [Test]
+        public void Fingerprint_ChangesWhenVertexChanges()
+        {
+            var mesh = new Mesh { vertices = new[] { Vector3.zero, Vector3.right, Vector3.forward }, triangles = new[] { 0, 1, 2 } };
+            var before = MeshFingerprint.Calculate(mesh);
+            var vertices = mesh.vertices;
+            vertices[0] = Vector3.up;
+            mesh.vertices = vertices;
+            var after = MeshFingerprint.Calculate(mesh);
+            Assert.AreNotEqual(before, after);
         }
     }
 }
